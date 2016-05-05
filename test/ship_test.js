8bdb4f555e186f3301f19dd7f33903c10d27f5c4
Janimations <janimations@gmx.de>
2016-05-05 15:02:27 -0700

    added another test

--- test/ship_test.js
+++ test/ship_test.js
@@ -1,19 +1,36 @@
 var expect = require('chai').expect;
 
-// check ship position
+// Suite for testing Ship Position:
 describe('checkForShip', function() {
 
-  // outline...
+  // Ship-methods:
   var checkForShip = require('../game_logic/ship_methods').checkForShip;    // "imports" the checkForShip function
 
+  // specs 01:
   it('should correctly report no ship at a given players coordinate', function() {
 
-      player = {
+      player = {                                // we need to supply a dummy-player here for the test to run...
           ships: [ { locations: [ [0, 0] ] } ]
       };
 
       expect(checkForShip(player, [9, 9])).to.be.false;
   });
 
+  // specs 02:
+  it('should correctly report a ship at the given coordinates', function() {
 
-});
+      player = {                                // we need to supply a dummy-player here for the test to run...
+          ships: [ { locations: [ [0, 0] ] } ]
+      };
+
+      expect(checkForShip(player, [0, ])).to.be.true;
+  });
+
+
+
+
+
+
+
+
+});     // describe close
