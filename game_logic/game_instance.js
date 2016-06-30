37604a3307714b3f297b9c17f261dcfe0bbc7a34
Janimations <janimations@gmx.de>
2016-06-30 16:44:28 -0700

    asynchronous test using 'done' is passing

--- game_logic/game_instance.js
+++ game_logic/game_instance.js
@@ -1,5 +1,22 @@
-function checkGameStatus (players) {
+var fire = require('../game_logic/ship_methods.js').fire;
 
+
+function checkGameStatus (players) {      // STILL NEEDS TO BE WRITTEN ...
+  // .... //
+  return false;   // has to return a boolean value
 }
 
+function takeTurn (opposingPlayer, guessFunction) {       // guessFuntion has not been written here, but as a "stub" in game_test.js inside the beforeEach-section
+  var coordinates = guessFunction();
+
+  fire(opposingPlayer, coordinates);
+
+  var gameOver = checkGameStatus();
+  return gameOver;
+}
+
+
+
+
 module.exports.checkGameStatus = checkGameStatus;
+module.exports.takeTurn = takeTurn;
--- test/game_test.js
+++ test/game_test.js
@@ -2,8 +2,10 @@ var expect = require('chai').expect;
 
 describe('GAME INSTANCE FUNCTIONS', function() {
 
+
     describe ('check Game Status', function () {
-        it('should tell me when the game is over', function(){     // this is a 'pending' test, so no 'function()' added yet...
+      var checkGameStatus = require ('../game_logic/game_instance.js').checkGameStatus;
+        it('should tell me when the game is over', function(){
 
         var players = [
         {
@@ -23,4 +25,54 @@ describe('GAME INSTANCE FUNCTIONS', function() {
     });
 
 
-});
+    describe('takeTurn', function () {
+      var takeTurn = require('../game_logic/game_instance.js').takeTurn;
+      var guess, player;
+
+      beforeEach(function () {
+
+          guess = function () { return [0, 0]; };     // guess is passed into takeTurn as a second parameter
+
+          player = {
+            ships: [
+              {
+                locations: [[0, 0]],
+                damage: []
+              }
+            ]
+          }
+      });
+
+      it('should return false if the game ends', function() {
+        var actual = takeTurn(player, guess);
+        expect(actual).to.be.false;
+      });
+
+    });
+
+
+
+    function saveGame (callback) {        // callback is a function that is passed through to setTmeout(), to be called after 1000ms
+      setTimeout(function () {            // setTimeout simulates an asynchronous call ...
+        callback();
+      }, 1000);
+    }
+
+
+    describe('saveGame', function () {
+
+      it('should update save status', function (done) {     // "done" as an argument signals mocha to wait checking expectations until "done" as a funciton is called in the code
+        var status = 'game not saved...';
+
+        saveGame(function() {
+          status = 'game saved!';   // this is the code that is called after setTimeout has waited 1000ms
+
+          expect(status).to.equal('game saved!');   // moved inside the callback and below the previous line, so that the expectation passes
+          done();
+        });
+
+      });  // it close
+    });   // describe close
+
+
+});   // suite close
